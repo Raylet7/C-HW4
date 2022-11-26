a3ecde67773acad416311189810631801540a173
Svetlana <tsvetcova.s@mail.ru>
2022-11-27 01:14:07 +0300

    add Project 1 Exponentiation

---
+++ Project1_Exponentiation/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа, принимающая числа А и В 
+// и возводящая число A в натуральную степень B
+// (используя цикл).
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int ExponentiationFor(int numberA, int exponentB)
+{
+    int result = 1;
+    for (int i = 1; i <= exponentB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+Console.WriteLine("Программа возведения числа в натуральную степень");
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральную степень: ");
+int exponentB = int.Parse(Console.ReadLine()!);
+if (exponentB < 1)
+{
+    Console.WriteLine("Неверный ввод");
+}
+else
+{
+    Console.WriteLine(numberA + "^" + exponentB + " = " + ExponentiationFor(numberA, exponentB));
+}
