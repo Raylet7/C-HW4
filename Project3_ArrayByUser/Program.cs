50599b6b388b69db1893742ec958692ebf7717fa
Svetlana <tsvetcova.s@mail.ru>
2022-11-27 18:27:15 +0300

    add Project 3 Array By User

---
+++ Project3_ArrayByUser/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, задающая массив из 8 целых чисел с клавиатуры 
+// и выводящая его на экран в одну строку.
+void FillArrayByUser(int[] array)
+{
+    int count = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {count}-е целое число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        count++;
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine("Программа создания пользователем массива из 8 целых чисел.");
+int[] array = new int[8];
+FillArrayByUser(array);
+Console.WriteLine("Заданный массив:");
+PrintArray(array);
+Console.WriteLine();
