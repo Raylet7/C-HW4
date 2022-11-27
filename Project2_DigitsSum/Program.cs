d2786d5226b88b27696171658f98b84252969357
Svetlana <tsvetcova.s@mail.ru>
2022-11-27 14:45:29 +0300

    add Project 2 Digits Sum

---
+++ Project2_DigitsSum/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, принимающая число и выдающая сумму цифр в числе. 
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int GetDigitsSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+Console.WriteLine("Программа определения суммы цифр числа.");
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number >= 0)
+{
+    Console.WriteLine("Сумма цифр числа равна: " + GetDigitsSum(number));
+}
+else
+{
+    number *= -1;
+    Console.WriteLine("Сумма цифр числа равна: " + GetDigitsSum(number));
+}
